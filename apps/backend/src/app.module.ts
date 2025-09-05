8a224c469c3d18d46ff22b372293bcab1e0a23f2
tenbite-daniel <tenbitedaniel60@gmail.com>
2025-09-05 21:48:36 +0300

    chore: fixed database connection

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TestController } from './test/test.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AppController, TestController],
   providers: [AppService],
 })
 export class AppModule {}
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { getConnectionToken } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 5000);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
+  const connection = app.get<Connection>(getConnectionToken());
+  connection.once('open', () => {
+    console.log('✅ MongoDB connected!');
+  });
+
+  await app.listen(process.env.PORT || 5000);
+  console.log(`Backend running on port ${process.env.PORT || 5000}`);
 }
 bootstrap();
